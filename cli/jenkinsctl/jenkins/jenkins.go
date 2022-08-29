da723de66f863a8a52d97e05e9df3a14bb4f554a
Rafflesiaceae <rafflesiaceae.plant@gmail.com>
2022-08-29 18:29:13 +0200

    Rename go mod

--- cli/jenkinsctl/jenkins/jenkins.go
+++ cli/jenkinsctl/jenkins/jenkins.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/bndr/gojenkins"
+	"github.com/Rafflesiaceae/gojenkinsplus"
 	"github.com/spf13/viper"
 	"io/ioutil"
 	"net/http"
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/bndr/gojenkins
+module github.com/Rafflesiaceae/gojenkinsplus
 
 go 1.17
 
